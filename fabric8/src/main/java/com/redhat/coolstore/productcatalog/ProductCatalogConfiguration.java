e4c1a4ee72843a0d6c4130e66e415817f8a21ef8
Tero Ahonen <tahonen@redhat.com>
2017-11-28 15:15:22 +0000

    Added fabric8 code

--- fabric8/src/main/java/com/redhat/coolstore/productcatalog/ProductCatalogConfiguration.java
+++ fabric8/src/main/java/com/redhat/coolstore/productcatalog/ProductCatalogConfiguration.java
@@ -1,4 +1,4 @@
-package com.redhat.emeatigers.springboot.service;
+package com.redhat.coolstore.productcatalog;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +8,12 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 @Configuration
-public class SpringBootConfiguration {
-
+public class ProductCatalogConfiguration {
+	
 	@Bean
 	public JacksonJsonProvider jsonProvider(ObjectMapper objectMapper) {
 		JacksonJaxbJsonProvider provider = new JacksonJaxbJsonProvider();
 		provider.setMapper(objectMapper);
 		return provider;
-    }
-
+	}
 }
